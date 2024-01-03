03f6844b0474ae07899b9b3b9b9557e79a2c989d
matheus <matheusrogato@gmail.com>
2024-01-03 19:41:04 -0300

    seção de perfil e descrição do usuário

--- Git_find/src/pages/Home/index.jsx
+++ Git_find/src/pages/Home/index.jsx
@@ -16,6 +16,15 @@ function App() {
               <input name="search-user" placeholder="@username" />
               <button type="button" className ="search-button">Buscar</button>
             </div>
+            <div className="profile">
+              <img className="profile-picture" src="https://avatars.githubusercontent.com/u/105300337?v=4" alt="foto-de-perfil" />
+              <div>
+                <h3>name</h3>
+                <span>@MatRogax</span>
+                <p>description</p>
+              </div>
+            </div>
+            <hr/>
           </div>
         </div>
       </div>
